8f882bd59545a160c38a8d0f35f0281891ac01be
Arodriguez1030 <arodriguez1030@gmail.com>
2021-04-16 22:44:44 -0400

    added categories onto page

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("in index.js");
+const catRoute = "http://localhost:3000/api/v1/categories";
+
+document.addEventListener("DOMContentLoaded", () => {
+    showCategory()
+});
+
+let showCategory = () => {
+    fetch(catRoute)
+    .then(response => response.json())
+    .then(cat => cat.data.forEach(category =>{
+        const catContainer = `
+        <div cat-id=${category.id}>
+        <h3>${category.attributes.name}</h3>
+        </div>`
+        
+        document.querySelector(".category-container").innerHTML+=catContainer;
+    })
+
+    )
+}
