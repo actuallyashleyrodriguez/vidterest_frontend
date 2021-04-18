c988533a3eee813cb8ac3085eb74f2710be5f1dd
Arodriguez1030 <arodriguez1030@gmail.com>
2021-04-18 08:20:19 -0400

    refactored renderCategory to use more JS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const catRoute = "http://localhost:3000/api/v1/categories";
+const vidRoute= "http://localhost:3000/api/v1/videos";
 
 document.addEventListener("DOMContentLoaded", () => {
     showCategory()
@@ -7,14 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
 let showCategory = () => {
     fetch(catRoute)
     .then(response => response.json())
-    .then(cat => cat.data.forEach(category =>{
-        const catContainer = `
-        <div cat-id=${category.id}>
-        <h3>${category.attributes.name}</h3>
-        </div>`
-        
-        document.querySelector(".category-container").innerHTML+=catContainer;
-    })
-
-    )
+    .then(cat => cat.data.forEach(category => renderCategory(category)))
 }
+
+let showVideos = () => {
+    fetch(vidRoute)
+    .then(response => response.json())
+    .then(vid => vid.data.forEach(video => renderVideo(video)))
+}
+
+let renderCategory = (catHash)=> {
+
+    const catDiv = document.createElement("div")
+    const h3 = document.createElement("h3")
+        
+    catDiv.setAttribute("cat-id", catHash.id)
+    h3.innerText = catHash.attributes.name
+        
+    catDiv.appendChild(h3)
+       
+    document.querySelector(".category-container").appendChild(catDiv);
+    
+    fetch(vidRoute)
+    .then(response => response.json())
+    .then(vid => vid.data.forEach(video => renderVideo(video)))
+        
+}
+
