7ca7dfa3cecfb75e907f7d9b5d1c58746470ab76
Arodriguez1030 <arodriguez1030@gmail.com>
2021-05-20 19:47:22 -0400

    refactored renderVideo function under Video  class

--- src/video.js
+++ src/video.js
@@ -4,5 +4,29 @@ class Video {
         this.description = data.description
         this.video_url = data.video_url
         this.category_id = data.category_id
+        Video.all.push(this)
+       
+    }
+    renderVideo() {
+        const div = document.querySelector(`div[id="${this.category_id}"] ul`)
+        const li  = document.createElement("li")
+        const subUL = document.createElement("ul")
+        const subLI = document.createElement("li")
+    
+        
+    //fetch request to OEMBED JSON on videos
+    
+        fetch(this.video_url)
+        .then(response => response.json())
+        .then(vidEmb => subLI.innerHTML = vidEmb.html)
+
+        li.innerText =  this.title
+        subUL.appendChild(subLI)
+        li.appendChild(subUL)
+
+        div.appendChild(li)
+
     }
 }
+
+Video.all= [];
